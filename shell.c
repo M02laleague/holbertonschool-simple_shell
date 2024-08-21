8c02f830784e026c8337037b616decc904cd769e
M02laleague <9558@holbertonstudents.com>
2024-08-21 17:39:26 +0200

    testing with the given fontions

--- shell.c
+++ shell.c
@@ -1,44 +1,161 @@
 #include "shell.h"
+/**
+ * read_and_divid - Reads user input and splits it into tokens
+ * @buffer: Buffer to store the command
+ * @argv: Array to store command arguments
+ * Return: 0 on success, -1 on failure
+ */
+int read_and_divid(char *buffer, char **argv)
+{
+	ssize_t b_read;
+	int i = 0;
+	char *token;
+
+	b_read = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+	if (b_read == -1)
+	{
+		perror("read");
+		return (-1);
+	}
+	if (b_read == 0)
+	{
+		return (-1);
+	}
+	buffer[b_read - 1] = '\0';
+
+	token = strtok(buffer, " ");
+	while (token != NULL && i < SIZE_ARG - 1)
+	{
+		argv[i++] = token;
+		token = strtok(NULL, " ");
+	}
+	argv[i] = NULL;
+	return (0);
+}
+
+/**
+ * handle_elements - Handles built-in commands
+ * @buffer: Input command
+ * @env: Environment variables
+ * Return: 1 if a built-in command was executed, 0 otherwise
+ */
+int handle_elements(char *buffer, char **env)
+{
+	if (strcmp(buffer, "exit") == 0)
+		return (1);
+
+	if (strcmp(buffer, "env") == 0)
+	{
+		while (*env)
+		{
+			printf("%s\n", *env);
+			env++;
+		}
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * find_command - Finds the full path of a command
+ * @cmd: Command to find
+ * Return: Full path of the command if found, NULL otherwise
+ */
+char *find_command(char *cmd)
+{
+	struct stat st;
+	char *path, *path_cpy, *dir;
+	static char full_path[BUFFER_SIZE];
+
+	if (stat(cmd, &st) == 0 && st.st_mode & S_IXUSR)
+		return (cmd);
+
+	path = getenv("PATH");
+	path_cpy = strdup(path);
+	dir = strtok(path_cpy, ":");
+
+	while (dir != NULL)
+	{
+		sprintf(full_path, "%s/%s", dir, cmd);
+		if (stat(full_path, &st) == 0 && st.st_mode & S_IXUSR)
+		{
+			free(path_cpy);
+			return (full_path);
+		}
+		dir = strtok(NULL, ":");
+	}
+	free(path_cpy);
+	return (NULL);
+}
+
+/**
+ * execute_command - Executes a command
+ * @cmd: Command to execute
+ * @argv: Command arguments
+ */
+void execute_command(char *cmd, char **argv)
+{
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return;
+	}
+	if (pid == 0)
+	{
+		if (execve(cmd, argv, environ) == -1)
+		{
+			perror("./shell");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		wait(&status);
+	}
+}
 
 /**
  * main - Entry point of the shell
- *
  * Return: Always 0
  */
-int	main(void)
+int main(void)
 {
-	char *buffer = NULL;/* stock la commande lue */
-	char *cmd;	 /* Pointeur pour stocker le chemin de la command*/
-	char *argv[SIZE_ARG];	 /* Tableau pour stoker les argument de la commande*/
-	size_t len = 0;
+	char buffer[BUFFER_SIZE];
+	char *argv[SIZE_ARG];
+	char *cmd;
 
-	while (1) /* boucle infinie pour maintenir le shell actif*/
+	while (1)
 	{
-		/*  Lire la commande entree et afichier le prompt*/
-		if (get_input_and_prompt(&buffer, &len, argv) == -1)
+		if (isatty(STDIN_FILENO))
+			printf("#cisfun$ ");
+		fflush(stdout);
+
+		if (read_and_divid(buffer, argv) == -1)
 		{
-			if (!isatty(STDIN_FILENO) || feof(stdin))
+			if (!isatty(STDIN_FILENO))
 				break;
 			continue;
 		}
 
-		/*gere le commande integrées*/
 		if (handle_elements(buffer, environ))
-		{
 			break;
-		}
-		cmd = find_command(argv[0]);/*le premier argument est une commande*/
-		/*Si la commande n'est pas un chemin absolu, la chercher dans le 'PATH'*/
-		if (cmd)
+
+		cmd = argv[0];
+		if (cmd[0] != '/' && cmd[0] != '.')
+			cmd = find_command(cmd);
+
+		if (cmd != NULL)
+		{
 			execute_command(cmd, argv);
+		}
 		else
 		{
-			printf("%s: command not found\n", argv[0]);
+			fprintf(stderr, "./shell: No such file or directory\n");
 		}
-
-		if (!isatty(STDIN_FILENO))/*sortir apres execution mode non interactif*/
-			break;
 	}
-	free(buffer); /*libere la memoire alouer par getline*/
-	return (0);	/*  le programe termine avec succes */
+	return (0);
 }
